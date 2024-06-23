5d7ae019b875ecf4fdeabc939fe14eed4040ea31
Rudolf Braun <48672663+BraunRudolf@users.noreply.github.com>
2024-06-23 19:25:28 +0200

    Fixing formatting

--- backend-app/app/sql_db/crud.py
+++ backend-app/app/sql_db/crud.py
@@ -7,7 +7,7 @@ from app.models import user as api_m
 from app.core.security import get_password_hashed
 from app.sql_db.database import SessionLocal
 
-# Dependency
+
 def get_db():
     db = SessionLocal()
     try:
@@ -15,12 +15,14 @@ def get_db():
     finally:
         db.close()
 
+
 def get_user(db: Session, user_id: int):
     return db.query(sql_m.User).filter(sql_m.User.id == user_id).first()
 
 
 def get_user_by_email(db: Session, email: str):
     return db.query(sql_m.User).filter(sql_m.User.email == email).first()
+
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(sql_m.User).offset(skip).limit(limit).all()
@@ -36,16 +38,18 @@ def create_user(db: Session, user: api_m.UserCreate):
 
 
 def update_is_admin(db: Session, user: api_m.User):
+    # TODO: ? Exceptin if user already admin?
     user.is_admin = True
     db.commit()
     return user
 
 
 def update_is_active(db: Session, user: api_m.User, new_state: bool):
-    user.is_active= new_state
+    # TODO: ? Exception if user has state already?
+    user.is_active = new_state
     db.commit()
     return user
+
+
 # TODO:
-# function set_inactive
-
-
+# function set_inactive necessary?
