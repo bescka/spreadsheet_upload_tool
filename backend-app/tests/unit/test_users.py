d4747ea6f40c2be466b3f2755acc4745c15448ba
Rudolf Braun <48672663+BraunRudolf@users.noreply.github.com>
2024-07-11 17:23:48 +0200

    Add further tests
    Tests for
    - read users
    - read user by mail

--- backend-app/tests/unit/test_users.py
+++ backend-app/tests/unit/test_users.py
@@ -64,8 +64,6 @@ def test_read_users_me_fault(unauth_client, db, users):
 
 def test_update_user_state_new(client, db, users):
     response = client.put("/users/me/update_state/", json={"new_state": False})
-    print("response")
-    print(response.json())
     assert response.status_code == 200
     assert response.json()["Message"] == "User status updated"
 
@@ -80,3 +78,48 @@ def test_update_user_state_same(unauth_client, db, users):
     response = unauth_client.put("/users/me/update_state/", json={"new_state": True})
     assert response.status_code == 401
     assert response.json()["detail"] == "Not authenticated"
+
+
+### Test read users
+def test_read_users_all(client_admin, db, users):
+    response = client_admin.get("/users/users")
+    assert response.status_code == 200
+    assert len(response.json()) == 3
+
+
+### TODO: check if necessary, since functionality already tested
+def test_read_users_skip(client_admin, db, users):
+    response = client_admin.get("/users/users", params={"skip": 1})
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
+
+def test_read_users_limit(client_admin, db, users):
+    response = client_admin.get("/users/users", params={"limit": 1})
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_read_users_skip_limit(client_admin, db, users):
+    response = client_admin.get("/users/users", params={"skip": 1, "limit": 2})
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
+
+def test_read_users_unauth(client, db, users):
+    response = client.get("/users/users")
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Not authenticated"
+
+
+### Test read user by mail
+def test_read_user_by_mail_ok(client_admin, db, users):
+    response = client_admin.get("/users/mail/user1@example.com")
+    assert response.status_code == 200
+    # WARNING: possible bottle neck for future changes
+    assert response.json() == {
+        "id": 1,
+        "email": "user1@example.com",
+        "is_active": True,
+        "is_admin": None,
+    }
