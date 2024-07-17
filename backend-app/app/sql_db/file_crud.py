a9f970a6c3a3dff4eb707d0c231363d01253e637
Rudolf Braun <48672663+BraunRudolf@users.noreply.github.com>
2024-07-17 14:20:12 +0200

    Remove unused function

--- backend-app/app/sql_db/file_crud.py
+++ backend-app/app/sql_db/file_crud.py
@@ -7,13 +7,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-def get_file_engine():
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL,
-        connect_args={
-            "check_same_thread": False
-        },  # WARNING:'check_same_thread only sqllite remove late
-    )
-    return engine
