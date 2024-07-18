acaf7385adbea6b55e891e7565d0c0810a265a3e
Rudolf Braun <48672663+BraunRudolf@users.noreply.github.com>
2024-07-18 12:06:24 +0200

    Add/modify tests

--- backend-app/tests/unit/conftest.py
+++ backend-app/tests/unit/conftest.py
@@ -1,14 +1,17 @@
 from fastapi.testclient import TestClient
+import pandas as pd
 import pytest
 from app.sql_db.database import Base
 from app.models.user import UserCreate
 from app.models.database import User as db_user
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+from io import BytesIO
 
 from app.main import app
 from app.api.auth import get_current_active_user, get_current_active_admin, get_user_by_email
 from app.sql_db.crud import create_user, get_db, update_is_active, update_is_admin
+from app.sql_db.file_crud import create_update_table, insert_data
 from app.models import user as api_m
 
 
@@ -26,17 +29,12 @@ def db_engine():
 
 @pytest.fixture(scope="function")
 def db(db_engine):
-    connection = db_engine.connect()
-
-    # WARNING: transaction not accessed
-    transaction = connection.begin()
-
-    db = Session(autocommit=False, autoflush=False, bind=connection)
-
-    yield db
-
-    db.rollback()
-    connection.close()
+    with db_engine.connect() as connection:
+        transaction = connection.begin()
+        db = Session(autocommit=False, autoflush=False, bind=connection)
+        yield db
+        transaction.rollback()
+        connection.close()
 
 
 @pytest.fixture(scope="function")
@@ -58,6 +56,7 @@ def client(db):
         is_active=True,
         is_admin=False,
     )
+    app.dependency_overrides[get_db] = lambda: db
     with TestClient(app) as test_client:
         yield test_client
     app.dependency_overrides = {}
@@ -85,7 +84,7 @@ def unauth_client(db):
         yield test_client
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def users(db):
     create_user(db, api_m.UserCreate(email="user1@example.com", password="test1"))
     create_user(db, api_m.UserCreate(email="user2@example.com", password="test2"))
@@ -98,11 +97,43 @@ def users(db):
 
 
 @pytest.fixture(scope="function")
+def file_db(db, files_good):
+
+    df = pd.read_csv(BytesIO(files_good["file"][1].encode()))
+    engine = db.get_bind().engine
+    filetable, msg = create_update_table(df, engine, "file_table")
+
+    insert_data(db, df, filetable)
+
+
+@pytest.fixture(scope="function")
 def files_good():
     files = {
         "file": (
             "test.csv",
-            "name,email,id,01.01.2024,02.01.2024\ntim, tim@kh.de,1,2,5\nmax, max@kh.de,2,3,20\ntina, tina@kh.de,3,30,2",
+            "name,email,person_id,01.01.2024,02.01.2024\ntim, tim@kh.de,1,2,5\nmax, max@kh.de,2,3,20\ntina, tina@kh.de,3,30,2",
+        )
+    }
+    return files
+
+
+@pytest.fixture(scope="function")
+def files_good_updated():
+    files = {
+        "file": (
+            "test.csv",
+            "name,email,person_id,01.01.2024,02.01.2024,03.01.2024\ntim, tim@kh.de,1,2,5,12\nmax, max@kh.de,2,3,20,20\ntina, tina@kh.de,3,30,2,3",
+        )
+    }
+    return files
+
+
+@pytest.fixture(scope="function")
+def files_bad_type():
+    files = {
+        "file": (
+            "test.txt",
+            "name,email,_person_id,01.01.2024,02.01.2024\ntim, tim@kh.de,1,2,5\nmax, max@kh.de,test,3,20\ntina, tina@kh.de,3,30,2",
         )
     }
     return files
@@ -113,7 +144,7 @@ def files_bad_dtype():
     files = {
         "file": (
             "test.csv",
-            "name,email,id,01.01.2024,02.01.2024\ntim, tim@kh.de,1,2,5\nmax, max@kh.de,test,3,20\ntina, tina@kh.de,3,30,2",
+            "name,email,_person_id,01.01.2024,02.01.2024\ntim, tim@kh.de,1,2,5\nmax, max@kh.de,test,3,20\ntina, tina@kh.de,3,30,2",
         )
     }
     return files
@@ -124,7 +155,7 @@ def files_bad_column():
     files = {
         "file": (
             "test.csv",
-            "fullname,email,id,01.01.2024,02.01.2024\ntim, tim@kh.de,1,2,5\nmax, max@kh.de,test,3,20\ntina, tina@kh.de,3,30,2",
+            "fullname,email,person_id,01.01.2024,02.01.2024\ntim, tim@kh.de,1,2,5\nmax, max@kh.de,test,3,20\ntina, tina@kh.de,3,30,2",
         )
     }
     return files
