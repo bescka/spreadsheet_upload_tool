0d721501e66391195255d2c22beddb8f1565ec6a
Rudolf Braun <48672663+BraunRudolf@users.noreply.github.com>
2024-07-25 18:38:49 +0200

    fix fix 3/3

--- backend-app/app/api/fileupload.py
+++ backend-app/app/api/fileupload.py
@@ -34,4 +34,4 @@ async def create_upload_file(
             raise HTTPException(status_code=422, detail="File needs to have .csv format.")
     except Exception as e:
         logger.error(f"Error: {str(e)}")
-        return e
+        raise e
