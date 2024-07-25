0487cf2e74432dded219342c1a8ee8a38f60ff54
Rudolf Braun <48672663+BraunRudolf@users.noreply.github.com>
2024-07-25 14:01:13 +0200

    Add postgres to compose

--- backend-app/app/sql_db/file_database.py
+++ backend-app/app/sql_db/file_database.py
@@ -1,18 +1,14 @@
 import os
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("FILE_DB_URL")
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={
-        "check_same_thread": False
-    },  # WARNING:'check_same_thread only sqllite remove late
 )
 SessionLocalFileDb = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
